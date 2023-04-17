b78d356d23b283ecac3ec803a2d5e82de85b90ae
bhnhat1709 <87702834+ulsyou@users.noreply.github.com>
2023-04-17 19:44:19 +0700

    commit

--- api/routes/admins.js
+++ api/routes/admins.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs'); // Thêm thư viện bcryptjs để mã hóa mật khẩu
+const bcrypt = require('bcryptjs'); 
 const Admin = require('../models/admin');
 const Staff = require('../models/staff');
 
 // Route thêm tài khoản admin mặc định
 router.post('/admin/create', async (req, res) => {
     try {
+        const { email, password } = req.body;
+        
+        // Hash mật khẩu trước khi lưu vào database
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Tạo mới admin với email và mật khẩu đã được mã hóa
         const admin = new Admin({
-            email: 'admin123@gmail.com',
-            password: 'admin123'
+            email,
+            password: hashedPassword
         });
 
         await admin.save();
@@ -19,8 +25,9 @@ router.post('/admin/create', async (req, res) => {
     }
 });
 
+
 // Route đăng nhập admin
-router.post('/admin/login', async (req, res) => {
+router.post('/admin/login', async (req, res, next) => {
     try {
         const { email, password } = req.body;
         const admin = await Admin.findOne({ email });
@@ -34,7 +41,9 @@ router.post('/admin/login', async (req, res) => {
         if (!isMatch) {
             return res.status(401).json({ message: 'Email or password incorrect' });
         }
-        if (admin.role === 'admin') {
+
+        // Kiểm tra email và mật khẩu là 'admin123@gmail.com' và 'admin123'
+        if (email === 'admin123@gmail.com' && password === 'admin123') {
             next();
         } else {
             return res.status(401).json({ message: 'No access permission' });
@@ -44,6 +53,7 @@ router.post('/admin/login', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
 
 // Middleware để kiểm tra quyền admin
 const isAdmin = async (req, res, next) => {
@@ -62,14 +72,16 @@ router.get('/staff', isAdmin, async (req, res) => {
 
 router.post('/staff', isAdmin, async (req, res) => {
     try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10); 
         const staff = new Staff({
+            _id: mongoose.Types.ObjectId(),
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             gender: req.body.gender,
             dateOfBirth: req.body.dateOfBirth,
             address: req.body.address,
             email: req.body.email,
-            password: req.body.password
+            password: hashedPassword 
         });
 
         await staff.save();
@@ -78,6 +90,7 @@ router.post('/staff', isAdmin, async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
 
 router.put('/staff/:id', isAdmin, async (req, res) => {
     try {
