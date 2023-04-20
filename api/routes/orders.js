22b4cf7c2e7e21a04b67f57983368468ba744eb8
bhnhat1709 <87702834+ulsyou@users.noreply.github.com>
2023-04-20 21:03:03 +0700

    commit

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,14 +8,16 @@ const CartItem = require("../models/cartItem");
 // Cấu hình Paypal API
 paypal.configure({
   mode: "sandbox", // Chế độ hoạt động của Paypal API (sandbox hoặc live)
-  client_id: "AQ0oPx58pxQxGQjX34ne0kUHcyvlHDe5-1oPAjRs3o15bxkwOPDO6_Q_MLkxU8P-8vO4cXZLFw2t9bJn", // Mã client ID của Paypal API
-  client_secret: "EKqo7_28R9DA-RV5uSTGlxgjG4xW67N7SLdClUWa7IhE7sr5vd9vlWvsNqwG4eZkxwWRO_k2f13VsMP1", // Mã client secret của Paypal API
+  client_id:
+    "AQ0oPx58pxQxGQjX34ne0kUHcyvlHDe5-1oPAjRs3o15bxkwOPDO6_Q_MLkxU8P-8vO4cXZLFw2t9bJn", // Mã client ID của Paypal API
+  client_secret:
+    "EKqo7_28R9DA-RV5uSTGlxgjG4xW67N7SLdClUWa7IhE7sr5vd9vlWvsNqwG4eZkxwWRO_k2f13VsMP1", // Mã client secret của Paypal API
 });
 
 // Route để thanh toán giỏ hàng
 router.post("/", async (req, res) => {
   try {
-    const { user, order, note, paymentType , address } = req.body;
+    const { user, order, note, paymentType, address } = req.body;
     if (paymentType === "Paypal") {
       // Xử lý phương thức thanh toán Paypal
       const create_payment_json = {
@@ -66,9 +68,12 @@ router.post("/", async (req, res) => {
       const newOrder = new Order({
         _id: new mongoose.Types.ObjectId(),
         user,
-        order : order.items.map(item => ({ product: item.product, quantity: item.quantity })),
+        order: order.items.map((item) => ({
+          product: item.product,
+          quantity: item.quantity,
+        })),
         address,
-        note, 
+        note,
         status: "Pending",
         paymentType,
       });
@@ -87,6 +92,50 @@ router.post("/", async (req, res) => {
         message: "Invalid payment type",
       });
     }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Something wrong please try again",
+      error: error.message,
+    });
+  }
+});
+
+/*
+API cập nhật trạng thái đơn hàng
+    {
+      "status": "Confirm"
+    }
+    {
+      "status": "Delivered"
+    }
+*/
+
+// Route để cập nhật status cho đơn hàng
+router.patch("/:orderId/status", async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    // Kiểm tra đơn hàng có tồn tại hay không
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found",
+      });
+    }
+
+    // Cập nhật status cho đơn hàng
+    order.status = status;
+    await order.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Order status updated successfully",
+      order,
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
