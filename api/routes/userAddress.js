af88d18d9c8bc9c01640f344bd9d760682829138
bhnhat1709 <87702834+ulsyou@users.noreply.github.com>
2023-04-22 09:12:58 +0700

    commit

--- api/routes/userAddress.js
+++ api/routes/userAddress.js
@@ -1,67 +1,68 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const User = require("../models/user");
 const UserAddress = require("../models/userAddress");
 
-//Thêm địa chỉ mới cho người dùng
-router.post("/create", async (req, res) => {
+// Thêm địa chỉ mới cho người dùng
+router.post("/:userId/create", async (req, res) => {
   try {
     const { address } = req.body;
     const newAddress = new UserAddress({
       _id: new mongoose.Types.ObjectId(),
-      address: address,
+      address,
+      user: req.params.userId,
     });
     const savedAddress = await newAddress.save();
+    const user = await User.findByIdAndUpdate(req.params.userId, { $push: { addresses: savedAddress._id } });
     res.status(201).json({ message: "Create new address successfully", data: savedAddress });
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: err.message });
   }
 });
 
-//Lấy thông tin địa chỉ của người dùng dựa trên ID
-router.get("/:id", async (req, res) => {
+//Lấy toàn bộ địa chỉ người dùng
+router.get("/:userId", async (req, res) => {
   try {
-    const address = await UserAddress.findById(req.params.id);
-    if (address) {
-      res.status(200).json({ data: address });
-    } else {
-      res.status(404).json({ message: "No Address found" });
-    }
+    const userAddresses = await UserAddress.find({ user: req.params.userId });
+    res.status(200).json({ data: userAddresses });
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: err.message });
   }
 });
 
-//Cập nhật thông tin địa chỉ của người dùng dựa trên ID
-router.put("/:id", async (req, res) => {
+
+// Cập nhật thông tin địa chỉ của người dùng dựa trên ID
+router.put("/:userId/:id", async (req, res) => {
   try {
     const { address } = req.body;
-    const updatedAddress = await UserAddress.findByIdAndUpdate(
-      req.params.id,
-      { address: address },
+    const updatedAddress = await UserAddress.findOneAndUpdate(
+      { _id: req.params.id, user: req.params.userId },
+      { address },
       { new: true }
     );
     if (updatedAddress) {
       res.status(200).json({ message: "Updated address successfully", data: updatedAddress });
     } else {
-      res.status(404).json({ message: "No addess found" });
+      res.status(404).json({ message: "No address found" });
     }
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: err.message });
   }
 });
 
-//Xóa địa chỉ của người dùng dựa trên ID
-router.delete("/:id", async (req, res) => {
+// Xóa địa chỉ của người dùng dựa trên ID
+router.delete("/:userId/:id", async (req, res) => {
   try {
-    const deletedAddress = await UserAddress.findByIdAndRemove(req.params.id);
+    const deletedAddress = await UserAddress.findOneAndRemove({ _id: req.params.id, user: req.params.userId });
     if (deletedAddress) {
+      await User.findByIdAndUpdate(req.params.userId, { $pull: { addresses: deletedAddress._id } });
       res.status(200).json({ message: "Deleted address successfully", data: deletedAddress });
     } else {
-      res.status(404).json({ message: "No addess found" });
+      res.status(404).json({ message: "No address found" });
     }
   } catch (err) {
-    res.status(500).json({ error: err });
+    res.status(500).json({ error: err.message });
   }
 });
 
