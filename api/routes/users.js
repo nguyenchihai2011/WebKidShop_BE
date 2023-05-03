3bd732220be2756c2fbddd19ebdcf1c6a8c0be21
bhnhat1709 <87702834+ulsyou@users.noreply.github.com>
2023-05-03 19:34:27 +0700

    commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -88,10 +88,6 @@ router.put("/:userId", (req, res, next) => {
   const id = req.params.userId;
   const updateOps = {};
 
-  if (!req.body.currentPassword) {
-    return res.status(400).json({ error: "Current password is required" });
-  }
-
   User.findById(id)
     .exec()
     .then((user) => {
@@ -99,43 +95,19 @@ router.put("/:userId", (req, res, next) => {
         return res.status(404).json({ message: "User not found" });
       }
 
-      bcrypt.compare(req.body.currentPassword, user.password, (err, result) => {
-        if (err) {
-          return res.status(500).json({ error: err });
+      for (const key of Object.keys(req.body)) {
+        if (key !== "username") {
+          updateOps[key] = req.body[key];
         }
+      }
 
-        if (!result) {
-          return res.status(401).json({ message: "Incorrect password" });
-        }
-
-        for (const key of Object.keys(req.body)) {
-          if (key !== "currentPassword" && key !== "username") {
-            updateOps[key] = req.body[key];
+      if (req.body.newPassword) {
+        bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({ error: err });
           }
-        }
+          updateOps.password = hash;
 
-        if (req.body.newPassword) {
-          bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
-            if (err) {
-              return res.status(500).json({ error: err });
-            }
-            updateOps.password = hash;
-
-            User.updateOne({ _id: id }, { $set: updateOps })
-              .exec()
-              .then((result) => {
-                res.status(200).json({
-                  message: "User updated",
-                });
-              })
-              .catch((error) => {
-                res.status(500).json({
-                  error: error,
-                });
-              });
-          });
-        } else {
-          // Update user in database
           User.updateOne({ _id: id }, { $set: updateOps })
             .exec()
             .then((result) => {
@@ -148,8 +120,22 @@ router.put("/:userId", (req, res, next) => {
                 error: error,
               });
             });
-        }
-      });
+        });
+      } else {
+        // Update user in database
+        User.updateOne({ _id: id }, { $set: updateOps })
+          .exec()
+          .then((result) => {
+            res.status(200).json({
+              message: "User updated",
+            });
+          })
+          .catch((error) => {
+            res.status(500).json({
+              error: error,
+            });
+          });
+      }
     })
     .catch((error) => {
       res.status(500).json({ error: error });
