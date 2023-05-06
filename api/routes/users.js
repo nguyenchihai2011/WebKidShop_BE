17c8d3abb13a57a7c129e72aa504d22f1d2b1a45
bhnhat1709 <87702834+ulsyou@users.noreply.github.com>
2023-05-06 14:51:57 +0700

    commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -90,52 +90,35 @@ router.put("/:userId", (req, res, next) => {
 
   User.findById(id)
     .exec()
-    .then((user) => {
+    .then(async (user) => {
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
 
       for (const key of Object.keys(req.body)) {
-        if (key !== "username") {
+        if (key !== "username" && key !== "password") {
           updateOps[key] = req.body[key];
         }
       }
 
-      if (req.body.newPassword) {
-        bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({ error: err });
-          }
-          updateOps.password = hash;
-
-          User.updateOne({ _id: id }, { $set: updateOps })
-            .exec()
-            .then((result) => {
-              res.status(200).json({
-                message: "User updated",
-              });
-            })
-            .catch((error) => {
-              res.status(500).json({
-                error: error,
-              });
-            });
-        });
-      } else {
-        // Update user in database
-        User.updateOne({ _id: id }, { $set: updateOps })
-          .exec()
-          .then((result) => {
-            res.status(200).json({
-              message: "User updated",
-            });
-          })
-          .catch((error) => {
-            res.status(500).json({
-              error: error,
-            });
-          });
+      if (req.body.password) {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        updateOps.password = hashedPassword;
       }
+
+      // Update user in database
+      User.updateOne({ _id: id }, { $set: updateOps })
+        .exec()
+        .then((result) => {
+          res.status(200).json({
+            message: "User updated",
+          });
+        })
+        .catch((error) => {
+          res.status(500).json({
+            error: error,
+          });
+        });
     })
     .catch((error) => {
       res.status(500).json({ error: error });
